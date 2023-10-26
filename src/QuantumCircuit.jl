09ed4419c42478189a2cbfe5fcfce40bfd338eaf
Adam Smith <adam.smith.home@ntlworld.com>
2023-10-26 20:31:52 +0100

    create file for default gates

--- src/QuantumCircuit.jl
+++ src/QuantumCircuit.jl
@@ -5,6 +5,7 @@ using TensorOperations
 include("tensors.jl")
 
 include("gates.jl")
+include("default_gates.jl")
 include("circuits.jl")
 include("states.jl")
 
---
+++ src/default_gates.jl
@@ -0,0 +1,3 @@
+function apply_hadamard(psi, qubits)
+
+end
